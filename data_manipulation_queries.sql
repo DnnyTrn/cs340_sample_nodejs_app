3041dbf183a4b8024b6cf6c57ec0adbab159a459
Danny <trandan@oregonstate.edu>
2019-02-27 11:17:43 -0800

    added new queries that are used in the app

---
+++ data_manipulation_queries.sql
@@ -0,0 +1,75 @@
+-- getHouses() - select all houses for get request
+SELECT h.id as house_id, name as house_name, sigil, c.fname, c.lname , hs.status FROM got_house h
+left join got_characters c on c.id = h.head
+left join got_house_status hs on hs.id = h.status_id;
+
+-- getSpecies() - select all species for get request
+select id as species_id, name as species_name from got_species
+
+-- select all characters and their rows 
+SELECT c.id, fname, lname, h.name as house_name, 
+l.name as location_name, weapon, status, organization, s.name as species_name FROM got_characters c
+left join got_house h on h.id = c.house_id
+left join got_locations l on l.id = c.origin
+left join got_species s on s.id = c.species_id;
+
+--functionality with colon : character being used to
+-- denote the variables that will have data from the backend programming language
+
+-- get all house IDs and names to populate the house dropdown in character insert form
+SELECT id, name FROM got_house
+
+-- get all location_IDs and names to populate the origin dropdown in character/house/event insert form
+-- getLocations()
+SELECT id, name FROM got_locations
+
+-- get all character IDs and names to populate the current head dropdown in location insert form
+SELECT id, name FROM got_characters
+
+-- get all characters and their cooresponding houses on the same character/house table
+SELECT got_characters.id, fname, lname, got_house.name AS house_id FROM got_characters INNER JOIN got_house ON house_id = got_house.id
+
+--get all events that one character is a part of
+SELECT e.name, c.fname, c.lname FROM got_events_characters ec INNER JOIN got_events e ON e.id = ec.event_id INNER JOIN got_characters c ON c.id = ec.character_id
+
+--get a single (character/house/location/event) data for the update form
+SELECT id, fname, lname, house_id, origin, weapon, species, status, organization FROM got_characters WHERE id = :character_row_selected_on_website_homepage
+SELECT id, name, status, head FROM got_house WHERE id = :house_row_selected_on_website_homepage
+SELECT id, name, region, continent FROM got_location WHERE id = :location_row_selected_on_website_homepage
+SELECT event_id, location, season, episode, summary FROM got_events WHERE event_id = :event_row_selected_on_website_homepage
+
+--associate a character with an event
+INSERT INTO got_events_characters (event_id, character_id) VALUES (:character_ID_from_dropdown, :event_id_from_dropdown)
+
+-- add a new character
+INSERT INTO got_characters (fname, lname, house_id, origin, weapon, species_id, status, organization) VALUES (?,?,?,?,?,?,?,?)
+
+-- add a new house
+INSERT INTO got_house (name, status, head) VALUES (:nameInput, :status_from_dropdown_Input, :head_id_from_dropdown_Input)
+
+-- add a new location
+INSERT INTO got_locations (name, region, continent) VALUES (:nameInput, :regionInput, :continent_from_dropdown_input)
+
+-- add a new event
+INSERT INTO got_events (name, location, season, episode, summary) VALUES (:nameInput, :location_id_from_dropdown_input, :seasonInput, :episodeInput, :summaryInput)
+
+-- update a character's data based on submission of the Update Character form
+UPDATE got_characters SET fname = :fnameInput, lname= :lnameInput, house = :house_id_from_dropdown_Input, origin = :origin_id_from_dropdown_Input, weapon = :weaponInput, species = :species_from_dropdown_Input, status = :status_from_dropdown_Input, organization = :organizationInput WHERE id = :character_ID_from_the_update_form
+
+-- update a houses's data based on submission of the Update House form
+UPDATE got_house SET name = :nameInput, status = :status_from_dropdown_Input, head = :head_id_from_dropdown_Input WHERE id = :house_ID_from_the_update_form
+
+-- update a location's data based on submission of the Update Location form
+UPDATE got_locations SET name = :nameInput, region = :regionInput, continent = :continent_from_dropdown_input WHERE id = :location_ID_from_the_update_form
+
+-- update a event's data based on submission of the Update Event form
+UPDATE got_events SET name = :nameInput, location = :location_id_from_dropdown_input, season = :seasonInput, epsiode = :episodeInput, summary = :summaryInput WHERE id = :event_ID_from_the_update_form
+
+-- delete a character/house/location/event
+DELETE FROM got_characters WHERE  id = :character_ID_selected_from_database_homepage
+DELETE FROM got_house WHERE id = :house_ID_selected_from_database_homepage
+DELETE FROM got_locations WHERE id = :location_ID_selected_from_database_homepage
+DELETE FROM got_events WHERE id = :event_ID_selected_from_database_homepage
+
+--Dis-associate an event from a person
+DELETE FROM got_events_characters WHERE character_id = :character_id_selected_from_character_and_events_list AND event_id = :events_id_selected_from_character_and_events_list
