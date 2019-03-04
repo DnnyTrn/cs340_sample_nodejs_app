6249ed08e48b3eb6cd1ce2c58ee12422080bef82
Danny <trandan@oregonstate.edu>
2019-03-04 15:28:19 -0800

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_trandan',
-  password        : '2023',
-  database        : 'cs340_trandan'
+  user            : 'cs340_wolford',
+  password        : '0000',
+  database        : 'cs340_wolford'
 });
 module.exports.pool = pool;
