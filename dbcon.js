55872fa3b527accc9e25320ae3fd79fb35442cc5
Danny Tran <dannyytran@gmail.com>
2019-03-04 15:27:05 -0800

    ignore dbocn

--- dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_trandan',
-  password        : '2023',
-  database        : 'cs340_trandan'
-});
-module.exports.pool = pool;
