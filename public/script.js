b300e5f1b9cde75458c8d38fcf9afb43179e4623
Danny <trandan@oregonstate.edu>
2019-03-03 22:30:16 -0800

    search function created

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+
 // this function manages the tooltips for the add and delete buttons 
 $(function () {
     $('[data-toggle="tooltip"]').on('click', function () {
@@ -8,28 +9,38 @@ $(function () {
 
 //ajax post request when user adds a new table row
 function add() {    
-    $(document).ready(
-        $.ajax({
-            url: window.location.pathname,
-            // url: '/character',
-            type: 'POST',
-            data: $('form').serialize(),
-            success: function (result, success, xhr) {
-                // window.location.replace("." + this.url); 
 
-                // create a new tbody with the new row using Handlebars script
-                console.log(result);
-                let rawTemplate = $("script[type='text/x-handlebars-template']").html();
-                var compiledTemplate = Handlebars.compile(rawTemplate);
-                var generatedHTML = compiledTemplate(result);
+    //the only post form on the page
+    const add_form = $("[method='post']");
+  
+    // check if name is whitespace or blank
+    if (!add_form[0][0].value.trim().length || add_form[0][0].validity.valueMissing)
+    {
+        // event.preventDefault();  
+        event.stopPropagation();  //display user required message
+    }else{
+        $(document).ready(
+            $.ajax({
+                url: window.location.pathname,
+                // url: '/character',
+                type: 'POST',
+                data: add_form.serialize(),   
+                success: function (result, success, xhr) {
+                    // window.location.replace("." + this.url); 
 
-                $('tbody').remove();
-                $('table').append(generatedHTML);
-                $('form').trigger("reset");
-            }
-        })
-        
-    )
+                    // create a new tbody with the new row using Handlebars script
+                    console.log(result);
+                    let rawTemplate = $("script[type='text/x-handlebars-template']").html();
+                    var compiledTemplate = Handlebars.compile(rawTemplate);
+                    var generatedHTML = compiledTemplate(result);
+
+                    $('tbody').remove();
+                    $('table').append(generatedHTML);
+                    $('form').trigger("reset");
+                }
+            })
+        )
+    }
 };
 
 // ajax function that accesses the delete request. 
@@ -49,3 +60,68 @@ function deleteRow(id) {
         })
     )
 };
+
+// search for all people with names like %
+// ajax get request to 'pagename/search/:name
+$('#search-button').on("click", function(){
+    this.disabled=true;
+
+        const search_form_id = $('#search-name');
+        const search_array = $('#search-name').serializeArray();
+        console.log(search_array);
+        console.log(search_array[0].value);
+        if (!search_array[0].value.trim().length)
+        {
+            console.log('Failed validation')
+            // event.preventDefault();  
+            event.stopPropagation();  //display user required message
+        }
+        else{
+            $(document).ready(
+                $.ajax({
+                    url: window.location.pathname + '/search/' + encodeURI(search_array[0].value),
+                    type: 'GET',
+                    success: (result, success, xhr) => {
+                        console.log(success);
+                        console.log(result);
+                        let rawTemplate = $("script[type='text/x-handlebars-template']").html();
+                        var compiledTemplate = Handlebars.compile(rawTemplate);
+                        var generatedHTML = compiledTemplate(result);
+
+                        $('tbody').remove();
+                        $('table').append(generatedHTML);
+
+                     
+                    }
+                })
+            )   
+        }
+
+        // enabled search button after 1 second
+    setTimeout(function () {
+        document.getElementById('search-button').disabled = false;
+    }, 1000);
+})
+
+// allows user to press enter and click to submit search
+    // search_form_id.unbind('submit').submit(function(event){
+                // insert code to prevent spamming requests 
+                //     event.preventDefault(); //prevents the form button from reloading the page
+
+                //     $.ajax({
+                //         url: window.location.pathname + '/search/' + search_form_id[0][0].value,
+                //         type: 'GET',
+                //         success: (result, success, xhr) =>{
+                //             console.log(success);
+                //             console.log(result);
+                //             let rawTemplate = $("script[type='text/x-handlebars-template']").html();
+                //             var compiledTemplate = Handlebars.compile(rawTemplate);
+                //             var generatedHTML = compiledTemplate(result);
+
+                //             $('tbody').remove();
+                //             $('table').append(generatedHTML);
+
+                //         }
+                //     })
+                // })    
+                // };
